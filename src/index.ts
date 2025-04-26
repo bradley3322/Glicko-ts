cec7dc3d97f2449b2a61b4b477a37082080df9aa
bradley3322 <bradleyrobinson332@gmail.com>
2025-04-26 21:19:50 +0100

    add index file: export Glicko class and related types for player, opponent, match, and configuration

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Main Glicko Calculator Class:
+export { Glicko } from './glicko';
+
+// Interfaces for Data Structures:
+export type { Player } from './interfaces/player';
+export type { Opponent } from './interfaces/opponent';
+export type { Match } from './interfaces/match';
+
+// Configuration Interface (if you decide to expose it):
+export type { GlickoConfig } from './config/glicko_config';
