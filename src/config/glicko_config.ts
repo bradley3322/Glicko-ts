bc69c9a957b457499a06cb233855ba710c466b04
bradley3322 <bradleyrobinson332@gmail.com>
2025-04-26 21:18:37 +0100

    add GlickoConfig interface for rating system configuration

---
+++ src/config/glicko_config.ts
@@ -0,0 +1,8 @@
+export interface GlickoConfig {
+    initialRating: number;
+    initialRD: number;
+    inactivityConstant: number;
+    rdCeiling: number;
+    q: number;
+    daysPerRatingPeriod: number;
+}
