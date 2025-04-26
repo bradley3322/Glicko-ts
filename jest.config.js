707cfdada6d818623577d0b61fbc92c5a6e304eb
bradley3322 <bradleyrobinson332@gmail.com>
2025-04-26 21:19:04 +0100

    update jest configuration: add coverage settings and specify test roots

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'], 
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/interfaces/**',
+    '!<rootDir>/src/types/**', 
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/constants.ts', 
+  ],
+};
