1e821831de4ee4b85293d9ddba44e1dbf65d1518
bradley3322 <bradleyrobinson332@gmail.com>
2025-04-26 21:19:32 +0100

    refactor MathUtils: convert g function to a static method within MathUtils class

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -1,8 +1,10 @@
-/**
- * Calculates the g(RD) function, which dampens the effect of the opponent's RD.
- * @param rd The rating deviation of the opponent.
- * @returns The dampened RD value.
- */
-function g(rd: number): number {
-    return 1 / Math.sqrt(1 + 3 * Math.pow(rd / Math.PI, 2));
+export class MathUtils {
+    /**
+     * Calculates the g(RD) function, which dampens the effect of the opponent's RD.
+     * @param rd The rating deviation of the opponent.
+     * @returns The dampened RD value.
+     */
+    static g(rd: number): number {
+        return 1 / Math.sqrt(1 + 3 * Math.pow(rd / Math.PI, 2));
+    }
 }
